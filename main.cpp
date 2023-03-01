f0d849760cd99b5fba8313ff64ca8a59fa62157f
Luciano Barletta <lushobarlett@gmail.com>
2023-03-01 15:23:44 -0300

    create new scene

--- main.cpp
+++ main.cpp
@@ -21,6 +21,8 @@ constexpr int HEIGHT = WIDTH / ASPECT_RATIO;
 constexpr int SAMPLES = 20;
 constexpr int MAX_RAY_DEPTH = 5;
 
+constexpr int SCENE = 1;
+
 vec3 ray_color(const ray& r, const hittable& world, int depth = 1) {
 	const vec3 white(1, 1, 1);
 	const vec3 lightblue(0.5, 0.7, 1);
@@ -42,64 +44,91 @@ vec3 ray_color(const ray& r, const hittable& world, int depth = 1) {
 	return vec3_lerp(white, lightblue, t);
 }
 
-hittable_list random_scene() {
-	hittable_list world;
+std::shared_ptr<lambertian> make_checker_texture() {
+	return std::make_shared<lambertian>(
+	    std::make_shared<checker_texture>(
+	        vec3(0.2, 0.3, 0.1), vec3(0.9, 0.9, 0.9)));
+}
 
-	auto checker = std::make_shared<checker_texture>(vec3(0.2, 0.3, 0.1), vec3(0.9, 0.9, 0.9));
-	auto ground_material = std::make_shared<lambertian>(checker);
-	world.objects.push_back(std::make_shared<sphere>(vec3(0, -1000, 0), 1000, ground_material));
+std::shared_ptr<lambertian> make_random_albedo() {
+	return std::make_shared<lambertian>(
+	    vec3_random() * vec3_random());
+}
+
+std::shared_ptr<dielectric> make_dielectric() {
+	return std::make_shared<dielectric>(1.5);
+}
+
+std::shared_ptr<sphere> make_small_sphere(vec3 center, std::shared_ptr<material> material) {
+	return std::make_shared<sphere>(center, 0.2, material);
+}
+
+hittable_list random_scene() {
+	hittable_list scene;
+
+	scene.objects.push_back(std::make_shared<sphere>(vec3(0, -1000, 0), 1000, make_checker_texture()));
 
 	for (int a = -11; a < 11; a++) {
 		for (int b = -11; b < 11; b++) {
-			auto choose_mat = random_double();
+
+			auto material_probability = random_double();
+
 			vec3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
 
 			if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
-				std::shared_ptr<material> sphere_material;
-
-				if (choose_mat < 0.8) {
-					// diffuse
-					auto albedo = vec3_random() * vec3_random();
-					sphere_material = std::make_shared<lambertian>(albedo);
-					auto moving_sphere = std::make_shared<sphere>(center, 0.2, sphere_material);
+				if (material_probability < 0.8) {
+					auto moving_sphere = make_small_sphere(center, make_random_albedo());
 					moving_sphere->velocity = vec3(0.0, 0.1, 0.0);
-					world.objects.push_back(moving_sphere);
-				} else if (choose_mat < 0.95) {
-					// metal
+					scene.objects.push_back(moving_sphere);
+
+				} else if (material_probability < 0.95) {
 					auto albedo = vec3_random(0.5, 1);
 					auto fuzz = random_double(0, 0.5);
-					sphere_material = std::make_shared<metal>(albedo, fuzz);
-					world.objects.push_back(std::make_shared<sphere>(center, 0.2, sphere_material));
+					auto metal_material = std::make_shared<metal>(albedo, fuzz);
+					scene.objects.push_back(make_small_sphere(center, metal_material));
+
 				} else {
-					// glass
-					sphere_material = std::make_shared<dielectric>(1.5);
-					world.objects.push_back(std::make_shared<sphere>(center, 0.2, sphere_material));
+					scene.objects.push_back(make_small_sphere(center, make_dielectric()));
 				}
 			}
 		}
 	}
 
-	auto material1 = std::make_shared<dielectric>(1.5);
-	world.objects.push_back(std::make_shared<sphere>(vec3(0, 1, 0), 1.0, material1));
+	scene.objects.push_back(std::make_shared<sphere>(vec3(0, 1, 0), 1.0, make_dielectric()));
 
 	auto material2 = std::make_shared<lambertian>(vec3(0.4, 0.2, 0.1));
-	world.objects.push_back(std::make_shared<sphere>(vec3(-4, 1, 0), 1.0, material2));
+	scene.objects.push_back(std::make_shared<sphere>(vec3(-4, 1, 0), 1.0, material2));
 
 	auto material3 = std::make_shared<metal>(vec3(0.7, 0.6, 0.5), 0.0);
-	world.objects.push_back(std::make_shared<sphere>(vec3(4, 1, 0), 1.0, material3));
+	scene.objects.push_back(std::make_shared<sphere>(vec3(4, 1, 0), 1.0, material3));
 
-	return world;
+	return scene;
+}
+
+hittable_list two_spheres() {
+	hittable_list scene;
+
+	scene.objects.push_back(std::make_shared<sphere>(vec3(0, -10, 0), 10, make_checker_texture()));
+	scene.objects.push_back(std::make_shared<sphere>(vec3(0,  10, 0), 10, make_checker_texture()));
+
+	return scene;
 }
 
 vec3 screen[WIDTH][HEIGHT];
 
-int main() {
-	hittable_list world = random_scene();
+hittable_list choose_scene(int scene) {
+	switch (scene) {
+		default:
+		case 1: return random_scene();
+		case 2: return two_spheres();
+	}
+}
 
+int main() {
 	double start_time = 0;
 	double end_time = 1;
 	
-	bvh_node bvh(world, start_time, end_time);
+	bvh_node bvh(choose_scene(SCENE), start_time, end_time);
 
 	camera c(vec3(13, 2, 3), vec3(0, 0, 0), vec3(0, 1, 0), M_PI / 8, ASPECT_RATIO, 0.1, 10);
 
